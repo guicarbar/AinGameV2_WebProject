b32fcbd284985ee5fb18263caedff12a066bf476
Guilherme Carvalho <162490839+guicarbar@users.noreply.github.com>
2024-10-22 22:16:03 -0300

    mudando a ideia do game

--- script/main.js
+++ script/main.js
@@ -1,46 +1,35 @@
-// Variável do peixe
+// variavel do peixe
 const icon = document.getElementById('icon')
 
-let iconX = 0
-let iconY = 0
-let targetX = 0
-let targetY = 0
-let animationFrame
-
-function animateMove() {
-    const delayFactor = 0.1
-    
-    iconX += (targetX - iconX) * delayFactor
-    iconY += (targetY - iconY) * delayFactor
-
-    icon.style.left = iconX + 'px'
-    icon.style.top = iconY + 'px'
-
-    animationFrame = requestAnimationFrame(animateMove)
-}
-
 function mouseMove(event) {
-    const iconWidth = icon.offsetWidth;
+    console.log ("icon movendo")
+
+    // largura e altura do peixe
+    const iconWidth = icon.offsetWidth
     const iconHeight = icon.offsetHeight
 
-    const maxX = window.innerWidth - iconWidth / 2
-    const maxY = window.innerHeight - iconHeight / 2
+    // largura e altura do navegador
+    const maxX = window.innerWidth - iconWidth
+    const maxY = window.innerHeight - iconHeight
 
-    let x = event.clientX
+    // posições do mous
+    let x = event.clientX;
     let y = event.clientY
 
+    // limite para eixo x
     if (x > maxX) {
         x = maxX
     }
 
+    // limite para eixo y
     if (y > maxY) {
         y = maxY
     }
 
-    targetX = x - iconWidth / 2
-    targetY = y - iconHeight / 2
+    // mover o peixe de posição
+    icon.style.left = x - 32 + 'px'
+    icon.style.top = y - 32 + 'px'
 }
 
-animateMove()
-
+// event listener
 document.addEventListener('mousemove', mouseMove)
